96911dfc7844ab2302a64559a532b6d5fa8eb3f3
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-04 02:08:31 +0000

    Run migration

--- db/migrate/20230704020421_create_operations.rb
+++ db/migrate/20230704020421_create_operations.rb
@@ -3,7 +3,7 @@ class CreateOperations < ActiveRecord::Migration[7.0]
     create_table :operations do |t|
       t.string :name
       t.decimal :amount
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.references :group, null: false, foreign_key: true
 
       t.timestamps
